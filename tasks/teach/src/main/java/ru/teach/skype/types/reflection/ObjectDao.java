5e65e4c915d8fddaf3e79a2eebbe741256769390
Kostegan <churilovkonstantin@gmail.com>
2016-01-13 01:36:49 +0400

    Adds new classes for a work with a database. Lesson 52

--- tasks/teach/src/main/java/ru/teach/skype/types/reflection/ObjectDao.java
+++ tasks/teach/src/main/java/ru/teach/skype/types/reflection/ObjectDao.java
@@ -6,6 +6,7 @@ import java.util.Map;
  *
  */
 public class ObjectDao {
+        //ClassName=TableName [nameField=TableName,nameField=TableName]
     public void save(Object object, String scheme) {
         String sql = "INSERT (name, age) VALUES ('Oleg', 25) INTO users";
 
@@ -21,5 +22,20 @@ public class ObjectDao {
 
         System.out.println("SQL: " + sb);
 
+    }
+
+    /**
+     * Divides a specified string and writes to array.
+     * index 0 - className, 1 - TableName, 2 - nameField1, 3 - TableField1, 4 - nameField2, 5 - TableField2
+     * @param scheme - the string which will be divided
+     */
+    public void parseScheme(String scheme){
+        String str = scheme;
+        String[] items = new String[6];
+        int temp = str.indexOf(61);
+        items[0] = str.substring(0,temp);
+        str = str.substring(temp+1);
+        temp = str.indexOf(32);
+        items[1] = str.substring(0,temp);
     }
 }
---
+++ tasks/teach/src/test/java/ru/teach/skype/types/reflection/ObjectDaoTest.java
@@ -0,0 +1,10 @@
+package ru.teach.skype.types.reflection;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+/**
+ *
+ */
+public class ObjectDaoTest {
+
+}
