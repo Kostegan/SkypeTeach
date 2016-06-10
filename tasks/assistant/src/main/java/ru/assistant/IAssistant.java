bb66dc1f32f5c4c49775f90d92a4018a7f3e9061
Kostegan <churilovkonstantin@gmail.com>
2016-06-10 20:06:40 +0400

    Renamed a method translateText on saveText

--- tasks/assistant/src/main/java/ru/assistant/IAssistant.java
+++ tasks/assistant/src/main/java/ru/assistant/IAssistant.java
@@ -3,9 +3,9 @@ package ru.assistant;
 /**
  * Class represents a basic construction, for working with a application.
  */
-public abstract class IAssistant {
+public class IAssistant {
 
-    public static void translateText() {
+    public static void saveText() {
 
     }
 }
