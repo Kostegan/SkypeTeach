a8cf4de2df63c5f8646c09ddfc4bb6a517711b6e
Kostegan <churilovkonstantin@gmail.com>
2016-06-27 14:08:08 +0400

    Add comments for this class

--- tasks/assistant/src/main/java/ru/assistant/items/translations/ITranslationFactory.java
+++ tasks/assistant/src/main/java/ru/assistant/items/translations/ITranslationFactory.java
@@ -1,7 +1,9 @@
 package ru.assistant.items.translations;
 
 /**
- *
+ * This class gets a set of words, translates this words and saved to a set of translations.
+ * Also need create a connection with a site for translation.
  */
 public interface ITranslationFactory {
+
 }
